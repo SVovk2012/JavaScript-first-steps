c9e45fa7ab767e777bb46a572d37df79e2d97b65
SVovk2012 <vovksergiyv@gmail.com>
2017-10-10 23:39:34 +0200

    Update MathGameBot.js

--- MathGameBot.js
+++ MathGameBot.js
@@ -1,6 +1,6 @@
 // PLEASE ADJUST required_result AND delay VARIABLES ACCORDING TO YOUR PREFERENCES. ENJOY! 
 var required_result = 32; // required result for the game
-var delay = 100; // delay in ms
+var delay = 100; // delay in ms.
 
 var restart = document.getElementById('button_correct');
 restart.click();
@@ -41,9 +41,48 @@ function runIt() {
   }
 };
 
+function runItReverse() {  // function for stoping the game. This function chooses the wrong result
+  var task_x = +document.getElementById('task_x').innerHTML;
+  var task_op = document.getElementById('task_op').innerHTML;
+  var task_y = +document.getElementById('task_y').innerHTML;
+  var task_res = document.getElementById('task_res').innerHTML;
+  
+  if (task_op == '+') {
+    if (task_x+task_y == task_res) {
+      doFalse();
+    }else {
+      doTrue();
+    }
+  }
+  if (task_op == '–') {
+    if (task_x-task_y == task_res) {
+      doFalse();
+    }else {
+      doTrue();
+    }
+  }
+  if (task_op == '/') {
+    if (task_x/task_y == task_res) {
+      doFalse();
+    }else {
+      doTrue();
+    }
+  }
+  if (task_op == '×') {
+    if (task_x*task_y == task_res) {
+      doFalse();
+    }else {
+      doTrue();
+    }
+  }
+};
+
 
 for(i = 0; i < required_result; i++){
  setTimeout(function(){runIt()}, i*delay); 
+}
+for(j = 0; j < 3; j++){
+setTimeout(function(){runItReverse()}, required_result*delay+5);
 }
 
 function doTrue() {
